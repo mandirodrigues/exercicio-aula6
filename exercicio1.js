abac7955cac8a584e8e85f5c5cc00ec1fc84d7b6
Amanda <amanda@MacBook-Pro-de-Felipe.local>
2025-07-04 17:26:35 -0300

    exerciciosaula6

---
+++ exercicio1.js
@@ -0,0 +1,8 @@
+
+const readline = require('readline-sync');
+
+let salario = readline.questionFloat('Digite o salário: ');
+let abono = readline.questionFloat('Digite o abono: ');
+let novoSalario = salario + abono;
+
+console.log('O novo salário é: ' + novoSalario);
